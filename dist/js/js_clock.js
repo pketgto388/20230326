df155e9c35e9087e235ca1c2602c410ecb7e48c7
pketgto388 <pketgto388@gmail.com>
2023-03-26 15:44:42 +0800

    20230326

--- dist/js/js_clock.js
+++ dist/js/js_clock.js
@@ -8,5 +8,20 @@ let timer = setInterval(() => {
     mm = mm.toString().padStart(2, '0');
     ss = ss.toString().padStart(2, '0');
 
+    let h1 = document.querySelector('#h-1');
+    let h2 = document.querySelector('#h-2');
+    h1.innerHTML = hh.substr(0, 1);
+    h2.innerHTML = hh.substr(1, 1);
+
+    let m1 = document.querySelector('#m-1');
+    let m2 = document.querySelector('#m-2');
+    m1.innerHTML = mm.substr(0, 1);
+    m2.innerHTML = mm.substr(1, 1);
+
+    let s1 = document.querySelector('#s-1');
+    let s2 = document.querySelector('#s-2');
+    s1.innerHTML = ss.substr(0, 1);
+    s2.innerHTML = ss.substr(1, 1);
+
     console.log(hh, mm, ss);
 }, 1000)
